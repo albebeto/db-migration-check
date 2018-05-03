a00ba3542c9c31ff4a3acaf32f52aa6e9d145f70
Alberto Colla <alberto.colla@garr.it>
2018-05-03 12:37:38 +0200

    test

--- setup_db.sh
+++ setup_db.sh
@@ -7,16 +7,16 @@ sed -i -e "s/connection = sqlite:\/\/\/\/var\/lib\/keystone\/keystone.db/connect
 sed -i -e "s/#connection = <None>/connection = mysql:\/\/root:${MYSQL_ROOT_PASSWORD}@${OPENSTACK_DB_HOST}\/glance/" "/etc/glance/glance-api.conf"
 
 #Set db in nova.conf
-echo "[database] \n\
-connection = mysql://root:${MYSQL_ROOT_PASSWORD}@${OPENSTACK_DB_HOST}/nova" >> /etc/nova/nova.conf
-echo "[api_database] \n\
-connection = mysql://root:${MYSQL_ROOT_PASSWORD}@${OPENSTACK_DB_HOST}/nova_api" >> /etc/nova/nova.conf
+echo "[database]" >> /etc/nova/nova.conf
+echo "connection = mysql://root:${MYSQL_ROOT_PASSWORD}@${OPENSTACK_DB_HOST}/nova" >> /etc/nova/nova.conf
+echo "[api_database]" >> /etc/nova/nova.conf
+echo "connection = mysql://root:${MYSQL_ROOT_PASSWORD}@${OPENSTACK_DB_HOST}/nova_api" >> /etc/nova/nova.conf
 
 #Set db in cinder.conf
 #RUN echo "[database] \n\
 #connection = mysql://root:my-secret-pw@127.0.0.1/cinder" >> /etc/cinder/cinder.conf
-echo "[database] \n\
-connection = mysql://root:${MYSQL_ROOT_PASSWORD}@${OPENSTACK_DB_HOST}/cinder" >> /etc/cinder/cinder.conf
+echo "[database]"  >> /etc/cinder/cinder.conf
+echo "connection = mysql://root:${MYSQL_ROOT_PASSWORD}@${OPENSTACK_DB_HOST}/cinder" >> /etc/cinder/cinder.conf
 
 #Set db in neutron.conf
 #RUN sed -i -e "s/connection = sqlite:\/\/\/\/var\/lib\/neutron\/neutron.sqlite/connection = mysql:\/\/root:my-secret-pw@127.0.0.1\/neutron/" "/etc/neutron/neutron.conf"
